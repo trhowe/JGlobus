459d678f3bfc071092b7057ef3ff996dffc4f869
Tom Howe <trhowe@uchicago.edu>
2010-07-14 11:04:59 -0500

    updated init.sh for developer support

--- init.sh
+++ init.sh
@@ -2,3 +2,25 @@
 
 git submodule init
 git submodule update
+
+function initDev {
+	cd gss
+	git remote set-url origin git@github.com:trhowe/JGlobus-GSS.git
+	cd ../ssl-proxies
+	git remote set-url origin git@github.com:trhowe/JGlobus-SSL-Proxies.git
+	cd ../ssl-proxies-tomcat
+	git remote set-url origin git@github.com:trhowe/JGlobus-SSL-Proxies-Tomcat.git
+	cd ../ssl-proxies-jboss
+	git remote set-url origin git@github.com:trhowe/JGlobus-SSL-Proxies-JBoss.git
+}
+
+while getopts ":d" dev; do
+	case $dev in
+		d)
+			initDev;
+			;;
+		\?)
+			echo "Unknown Option: -$OPTARG" >&2
+			;;
+	esac
+done 
